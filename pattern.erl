9d3299c489b9cfd251b6e2dd02a28db2dcd4afa6
Arkadiusz Plichta <el.pikel@gmail.com>
2017-02-20 18:54:17 +0100

    pattern matching ex

---
+++ pattern.erl
@@ -0,0 +1,20 @@
+-module(pattern).
+-export ([maxThree/3,howManyEqual/3]).
+
+maxThree(A,B,C) when A >= B, A >= C ->
+  A;
+maxThree(A,B,C) when B >= A, B >= C ->
+  B;
+maxThree(A,B,C) when C >= A, C >= B ->
+  C.
+
+howManyEqual(A,A,A) ->
+  3;
+howManyEqual(A,A,_) ->
+  2;
+howManyEqual(A,_,A) ->
+  2;
+howManyEqual(_,A,A) ->
+  2;
+howManyEqual(_,_,_) ->
+  0.
