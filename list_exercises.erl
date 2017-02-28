99e13f410fc6a7b3e295e8aa2b937683af69ba56
Arkadiusz Plichta <el.pikel@gmail.com>
2017-02-28 15:29:48 +0100

    nub

--- list_exercises.erl
+++ list_exercises.erl
@@ -1,5 +1,5 @@
 -module(list_exercises).
--export([double/1,evens/1]).
+-export([double/1,evens/1,nub/1]).
 
 double([]) -> [];
 double([X|Xs]) -> double(Xs, [X*2]).
@@ -12,4 +12,17 @@ evens([_Odd,Even|Rest]) -> evens(Rest, [Even]).
 
 evens([], Evens) -> Evens;
 evens([_Odd,Even|Rest], Evens) -> evens(Rest, [Even|Evens]);
-evens([Odd], Evens) -> Evens.
+evens(_Odd, Evens) -> Evens.
+
+nub([]) -> [];
+nub([X|Xs]) -> nub(Xs, [X]).
+
+nub([], N) -> lists:reverse(N, []);
+nub([X|Xs], Ns) ->
+  case exists_in(X, Ns) of
+    true -> nub(Xs, Ns);
+    false -> nub(Xs, [X|Ns])
+  end.
+
+exists_in(X, Ns) ->
+  lists:any(fun(N) -> N == X end, Ns).
