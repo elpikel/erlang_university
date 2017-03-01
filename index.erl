1525068df688c2817a474d447ece5f87ff023e3a
Arkadiusz Plichta <arpl@Arkadiuszs-MacBook-Pro.local>
2017-03-01 22:53:10 +0100

    index

--- index.erl
+++ index.erl
@@ -1,13 +1,44 @@
 -module(index).
--export([get_file_contents/1,show_file_contents/1,get_words_from_lines/1]).
+-export([get_file_contents/1,show_file_contents/1,get_words_with_line_number/1]).
 
 
-get_words_from_lines([]) -> [];
-get_words_from_lines([Line|Lines]) ->
-  lists:append(get_words_from_line(Line), get_words_from_lines(Lines)).
+% index:get_words_with_line_number(Content).
+get_words_with_line_number([]) -> [];
+get_words_with_line_number(Lines) ->
+  group_by_word(
+    sort_by_word(
+      get_words_with_line_number(Lines, 1))).
 
-get_words_from_line([]) -> [];
-get_words_from_line(Words) -> re:split(Words," ",[{return,list}]).
+get_words_with_line_number([], _LineNumber) -> [];
+get_words_with_line_number([Line|Lines], LineNumber) ->
+  lists:append(get_words_with_line_number(Lines, LineNumber + 1),
+              get_words_from_line(Line, LineNumber)).
+
+get_words_from_line([], _LineNumber) -> [];
+get_words_from_line(Line, LineNumber) ->
+  add_line_number(re:split(Line," ",[{return,list}]), LineNumber).
+
+add_line_number([], _LineNumber) -> [];
+add_line_number([Word|Words], LineNumber) ->
+  [{string:to_lower(Word), [LineNumber]}] ++ add_line_number(Words, LineNumber).
+
+sort_by_word(Words) ->
+  lists:sort(
+    fun({Word1, _LineNumber1}, {Word2, _LineNumber2}) -> Word1 < Word2 end,
+    Words).
+
+group_by_word([]) -> [];
+group_by_word(WordsWithLineNumber) ->
+  group_by_word(WordsWithLineNumber, []).
+
+group_by_word([], WordsWithLineNumbers) -> WordsWithLineNumbers;
+group_by_word([{Word, LineNumber}|WordsWithLineNumber], []) ->
+  group_by_word(WordsWithLineNumber, [{Word, LineNumber}]);
+group_by_word([{Word, LineNumbers}|WordsWithLineNumber], [{LastWord, LastLineNumbers}|WordsWithLineNumbers]) ->
+  case Word == LastWord of
+    true -> group_by_word(WordsWithLineNumber,[{Word, LastLineNumbers ++ LineNumbers}|WordsWithLineNumbers]);
+    false -> group_by_word(WordsWithLineNumber,[{Word, LineNumbers},{LastWord, LastLineNumbers}|WordsWithLineNumbers])
+  end.
 
 % Used to read a file into a list of lines.
 % Example files available in:
