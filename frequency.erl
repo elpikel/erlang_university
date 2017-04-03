6c72530891d0c9ca5d927a45a775c66a66a06717
Arkadiusz Plichta <el.pikel@gmail.com>
2017-04-03 21:19:00 +0200

    refactor of frequency server

---
+++ frequency.erl
@@ -0,0 +1,63 @@
+%% Based on code from
+%%   Erlang Programming
+%%   Francecso Cesarini and Simon Thompson
+%%   O'Reilly, 2008
+%%   http://oreilly.com/catalog/9780596518189/
+%%   http://www.erlangprogramming.org/
+%%   (c) Francesco Cesarini and Simon Thompson
+
+-module(frequency).
+-export([init/0,server/0]).
+
+server() ->
+  Pid = spawn(frequency, init, []),
+  register(frequency, Pid).
+
+%% These are the start functions used to create and
+%% initialize the server.
+
+init() ->
+  Frequencies = {get_frequencies(), []},
+  loop(Frequencies).
+
+% Hard Coded
+get_frequencies() -> [10,11,12,13,14,15].
+
+%% The Main Loop
+
+loop(Frequencies) ->
+  receive
+    {request, Pid, allocate} ->
+      {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+      Pid ! {reply, Reply},
+      loop(NewFrequencies);
+    {request, Pid , {deallocate, Freq}} ->
+      NewFrequencies = deallocate(Frequencies, Freq, Pid),
+      Pid ! {reply, ok},
+      loop(NewFrequencies);
+    {request, Pid, stop} ->
+      Pid ! {reply, stopped}
+  end.
+
+%% The Internal Help Functions used to allocate and
+%% deallocate frequencies.
+
+allocate({[], Allocated}, _Pid) ->
+  {{[], Allocated}, {error, no_frequency}};
+allocate({[Freq|Free], Allocated}, Pid) ->
+  case is_allocated(Pid, Allocated) of
+    true  -> {{Free, Allocated}, {cannot_alocate_more_than_one}};
+    false -> {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}
+  end.
+
+deallocate({Free, Allocated}, Freq, Pid) ->
+  case is_allocated(Pid, Allocated) of
+    true  ->
+      NewAllocated=lists:keydelete(Freq, 1, Allocated),
+      {[Freq|Free],  NewAllocated};
+    false ->
+      {Free, Allocated}
+  end.
+
+is_allocated(Pid, Allocated) ->
+  lists:any(fun({_FreqToCheck, PidToCheck}) -> Pid == PidToCheck end, Allocated).
